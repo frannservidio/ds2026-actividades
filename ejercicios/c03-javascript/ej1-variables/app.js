09db9661e23749cb8be72224bacf2537343c4024
Francisco <franciscogservidio@gmail.com>
2026-04-14 10:28:55 -0300

    Ejercicio 1: variables y operaciones

---
+++ ejercicios/c03-javascript/ej1-variables/app.js
@@ -0,0 +1,12 @@
+const nombre = "Francisco Gabriel Servidio";
+const edad = 19;
+const materia = "Desarrollo de Software";
+
+console.log(`Me llamo ${nombre}, tengo ${edad} años y curso ${materia}`);
+
+let contador = 0;
+contador += 1;
+contador += 1;
+contador += 1;
+
+console.log(`Valor final del contador: ${contador}`);
